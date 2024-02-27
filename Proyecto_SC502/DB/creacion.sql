3912fdea6bd1b5fa0fd77e808152868c7360307e
rugamac <105762180+rugamac@users.noreply.github.com>
2024-02-27 01:24:17 -0600

    Cambios y creacion

--- Proyecto_SC502/DB/creacion.sql
+++ Proyecto_SC502/DB/creacion.sql
@@ -1,4 +1,4 @@
-create table estudiante
+create table usuario
 (
     id INT(11) NOT NULL AUTO_INCREMENT, 
     nombre VARCHAR(50) NOT NULL,
@@ -8,10 +8,19 @@ create table estudiante
     PRIMARY KEY(id)
 );
 
+-- Crear la tabla "rutinas"
+CREATE TABLE IF NOT EXISTS rutinas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    nombre_cliente VARCHAR(255),
+    fecha DATE,
+    hora TIME,
+    descripcion TEXT
+);
 
 -- generado con la herramienta grafica
 
-CREATE TABLE `universidad`.`prueba` 
+CREATE TABLE `Gimnasio`.`prueba` 
 (`id` INT(11) NOT NULL AUTO_INCREMENT , 
 `nombre` VARCHAR(50) NOT NULL , 
 `correo` VARCHAR(50) NOT NULL , 
---
+++ Proyecto_SC502/creaRutina.php
@@ -0,0 +1,67 @@
+<?php
+$login = false;
+require_once "include/templates/headerClientes.php";
+
+
+<main class="contenedor">
+    <h1>Listar clientes</h1>
+
+    <div>
+        <p><?= $ingreso==1? "Se ingreso la rutina correctamente" : "" ; ?></p>
+
+        <?php
+        require_once "DAL/cliente.php";
+        $elSQL = "select id, nombre, correo, telefono from clientes";
+        $myArray = getArray($elSQL);
+
+        // var_dump($myArray);
+        // echo json_encode($myArray, JSON_UNESCAPED_UNICODE);
+        ?>
+
+        <div>
+            <h2>Datos de los Clientes</h2>
+            <table width=90%>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    <th>Teléfono</th>
+                    <th>Edad</th>
+                    <th>Antecedentes médicos</th>
+                    <th>Tiempo de actividad</th>
+                    <th>Embarazos o cirugías</th>
+                    <th>Subscripción</th>
+                </tr>
+                <?php
+                if(!empty($myArray)){
+                    foreach ($myArray as $value) {
+                        echo "<tr>";
+                        echo "<td>" . $value['nombre'] . "</td>";
+                        echo "<td>" . $value['correo'] . "</td>";
+                        echo "<td>{$value['telefono']}</td>";
+                        echo "<td>{$value['edad']}</td>";
+                        echo "<td>{$value['antecedentes médicos']}</td>";
+                        echo "<td>{$value['tiempo de actividad']}</td>";
+                        echo "<td>{$value['embarazos o cirugias']}</td>";
+                        echo "<td>{$value['Subscripcion']}</td>";
+
+                        echo "<td><a href=mostrarCliente.php?id={$value['id']}>Ver</a></td>";
+                        echo "<td><a href=editarCliente.php?id={$value['id']}>Editar</a></td>";
+                        
+                        echo "</tr>";
+                    }
+                }else{
+                    echo "<tr><td>No hay registros de Rutina</td></tr>";
+                }
+                ?>
+
+            </table>
+        </div>
+
+
+    </div>
+</main>
+
+<?php
+// include_once "include/templates/header.php";
+include "include/templates/footer.php";
+?>
