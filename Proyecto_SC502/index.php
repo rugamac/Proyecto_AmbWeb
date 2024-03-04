51e1fa0caede424a8d767f3d8f7774f6ed79bcda
Daniel <dbarquero70479@ufide.ac.cr>
2024-03-04 17:56:51 -0600

    index
    iconos font awesome

--- Proyecto_SC502/index.php
+++ Proyecto_SC502/index.php
@@ -21,6 +21,9 @@
     <title>Index</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
@@ -38,12 +41,12 @@
                 </tr>
             </thead>
             <tbody>
-                <tr> 
+                <tr>
                     <td>Mark</td>
                     <td>desactivado</td>
                     <td>
-                        <a href="" >Perfil</a>
-                        <a href="" >Sistema</a>
+                        <a href="" class="btn btn-success btn-lg"><i class="fa-solid fa-dumbbell"></i>Perfil</a>
+                        <a href="" class="btn btn-warning btn-lg"><i class="fa-solid fa-desktop"></i>Sistema</a>
                     </td>
                 </tr>
             </tbody>
