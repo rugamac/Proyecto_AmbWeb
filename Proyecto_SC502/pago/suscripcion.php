46ebea8ecee3bc21ba2da80696b3f73c775e9891
rugamac <105762180+rugamac@users.noreply.github.com>
2024-04-12 21:26:04 -0600

    Update suscripcion.php

--- Proyecto_SC502/pago/suscripcion.php
+++ Proyecto_SC502/pago/suscripcion.php
@@ -3,12 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gestión de Usuarios</title>
+  <title>Usuarios</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
   <div class="container">
-    <h1>Gestión de Usuarios</h1>
+    <h1>Usuarios</h1>
     <div class="btn-group">
       <button class="btn btn-primary" id="agregarUsuario">Agregar Usuario</button>
     </div>
