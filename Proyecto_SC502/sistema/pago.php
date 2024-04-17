630a50d81eabe934b8a924cc3787eb2914dee7e5
Daniel <dbarquero70479@ufide.ac.cr>
2024-04-17 16:06:13 -0600

    cambios 2

--- Proyecto_SC502/sistema/pago.php
+++ Proyecto_SC502/sistema/pago.php
@@ -11,9 +11,9 @@
 </head>
 
 <body>
-    
+
     <div class="d-flex justify-content-between m-5">
-        <div class="card border-0 bg-white" style="width:400px" >
+        <div class="card border-0 bg-white" style="width:400px">
             <a href="#paymentModal" data-toggle="modal" class="text-decoration-none text-primary">
                 <img class="card-img-top"
                     src="https://grupokankun.com/wp-content/uploads/2020/02/primera-tarjeta-de-cre%CC%81dito.jpg"
@@ -26,7 +26,8 @@
         <br>
 
         <div class="card border-0 bg-white" style="width:400px">
-            <a href="https://www.paypal.com/cr/webapps/mpp/payment-method" target="_blank" class="text-decoration-none text-primary">
+            <a href="https://www.paypal.com/cr/webapps/mpp/payment-method" target="_blank"
+                class="text-decoration-none text-primary">
                 <img class="card-img-top"
                     src="https://pics.paypal.com/00/s/NjQwYjhjNTktNDFhZi00OGRkLWI2MzgtMmMxZmE3OTY5YzU0/file.PNG"
                     alt="Card image" style="width:100%">
@@ -38,7 +39,8 @@
         <br>
 
         <div class="card border-0 bg-white" style="width:400px">
-            <a href="#" id="sinpeLink" data-toggle="modal" data-target="#sinpeModal" class="text-decoration-none text-primary">
+            <a href="#" id="sinpeLink" data-toggle="modal" data-target="#sinpeModal"
+                class="text-decoration-none text-primary">
                 <img class="card-img-top"
                     src="https://play-lh.googleusercontent.com/QweCEZ_R81jSZMwQlBvX6ldqiNOZbJudyzJ6HcMOPuNt0XsjmRcXyJVWA6R7lv3RV9o"
                     alt="Card image" style="width:100%">
@@ -48,6 +50,12 @@
             </a>
         </div>
         <br>
+    </div>
+
+    <div class="mt-4 p-5 bg-dark text-white">
+        <h1 class="text-center text-success">Nota:</h1>
+        <hr class="border-top border-success opacity-25 my-2">
+        <h3 class="text-center text-white">Contactar con la entrenadora para confirmar pago y active la suscripcion</h3>
     </div>
 
     <!-- Modal -->
@@ -91,25 +99,27 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script>
-        $(document).ready(function () {
-            $('#sinpeButton').popover();
-            $("#payButton").click(function () {
-                // Aquí podrías agregar la lógica para procesar el pago
-                // Puedes obtener los valores de los campos de entrada usando jQuery o JavaScript vanilla
-                var cardNumber = $("#cardNumber").val();
-                var expiry = $("#expiry").val();
-                var cvv = $("#cvv").val();
-                if (!cardNumber || !expiry || !cvv) {
-                    alert("Por favor complete los campos para relizar el pago!");
-                    return;
-                }
-                // Aquí deberías hacer la validación de la tarjeta y el proceso de pago
-                // Por simplicidad, aquí simplemente mostramos una alerta
-                alert("Pago procesado exitosamente. Se ha enviado una nota de confirmación a su correo electrónico.");
-                // Cierra el modal después del pago
-                $("#paymentModal").modal('hide');
-            });
+    $(document).ready(function() {
+        $('#sinpeButton').popover();
+        $("#payButton").click(function() {
+            // Aquí podrías agregar la lógica para procesar el pago
+            // Puedes obtener los valores de los campos de entrada usando jQuery o JavaScript vanilla
+            var cardNumber = $("#cardNumber").val();
+            var expiry = $("#expiry").val();
+            var cvv = $("#cvv").val();
+            if (!cardNumber || !expiry || !cvv) {
+                alert("Por favor complete los campos para relizar el pago!");
+                return;
+            }
+            // Aquí deberías hacer la validación de la tarjeta y el proceso de pago
+            // Por simplicidad, aquí simplemente mostramos una alerta
+            alert(
+                "Pago procesado exitosamente. Se ha enviado una nota de confirmación a su correo electrónico."
+                );
+            // Cierra el modal después del pago
+            $("#paymentModal").modal('hide');
         });
+    });
     </script>
     <!-- SINPE Modal -->
     <div class="modal fade" id="sinpeModal" tabindex="-1" role="dialog" aria-labelledby="sinpeModalLabel"
@@ -124,7 +134,8 @@
                 </div>
                 <div class="modal-body">
                     <p>El número SINPE para realizar la transferencia es: <strong>########</strong></p>
-                    <p>Para confirmar su compra de suscripción y para contactar con la entrenadora, puede llamar al número: <strong>########</strong></p>
+                    <p>Para confirmar su compra de suscripción y para contactar con la entrenadora, puede
+                        llamar al número: <strong>########</strong></p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
